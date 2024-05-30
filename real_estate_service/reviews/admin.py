d2f7e5c56865a0b15c33acbf948b68d2d1cb4676
OFF1GHT <kotik.d31sha@yandex.ru>
2024-05-30 17:04:03 +0300

    Дораюотала модель комментариев

--- real_estate_service/reviews/admin.py
+++ real_estate_service/reviews/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Review
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['author', 'real_estate', 'text', 'is_moderated', 'created_at', 'updated_at']
-    list_filter = ['is_moderated', 'created_at']
+    list_display = ['author', 'real_estate', 'text', 'status', 'created_at', 'updated_at']
+    list_filter = ['status', 'created_at']
     search_fields = ['author__username', 'real_estate__title']
 
 admin.site.register(Review, ReviewAdmin)
--- real_estate_service/reviews/models.py
+++ real_estate_service/reviews/models.py
@@ -2,20 +2,22 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from object.models import RealEstate
 from django.conf import settings
-from django.db.models.signals import pre_delete
-from django.dispatch import receiver
 
-class DeletedUser(models.Model):
-    username = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.username
 
 class Review(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    class ReviewStatus(models.TextChoices):
+        PENDING = 'P', _('Pending')
+        APPROVED = 'A', _('Approved')
+        REJECTED = 'R', _('Rejected')
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
     real_estate = models.ForeignKey(RealEstate, related_name='reviews', on_delete=models.CASCADE)
     text = models.TextField()
-    is_moderated = models.BooleanField(default=False)
+    status = models.CharField(
+        max_length=1,
+        choices=ReviewStatus.choices,
+        default=ReviewStatus.PENDING,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -24,8 +26,3 @@ class Review(models.Model):
             return f'Review by {self.author} for {self.real_estate}'
         else:
             return f'Review for {self.real_estate}'
-
-@receiver(pre_delete, sender=settings.AUTH_USER_MODEL)
-def set_deleted_user(sender, instance, **kwargs):
-    deleted_user, _ = DeletedUser.objects.get_or_create(username="Deleted User")
-    Review.objects.filter(author=instance).update(author=deleted_user)
