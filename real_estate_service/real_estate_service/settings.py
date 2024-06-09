55b07cc7cd992e8210e52d199f95cb82fa2fd399
Софья Боровкова <borovkova.sonya.sonya66@gmail.com>
2024-06-10 00:59:40 +0300

    fix

--- real_estate_service/real_estate_service/settings.py
+++ real_estate_service/real_estate_service/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'real_estate_service.urls'
