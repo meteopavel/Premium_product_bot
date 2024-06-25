358fd07bbfe057f6dc4fe74b1e74bc09cbd05ebe
OFF1GHT <kotik.d31sha@yandex.ru>
2024-06-25 16:20:15 +0300

    Добавлен график работы

--- real_estate_service/object/models.py
+++ real_estate_service/object/models.py
@@ -258,3 +258,36 @@ class AreaIntervals(BaseIntervals):
                 f"Only up to {MAX_INTERVALS_COUNT} Area Intervals are allowed."
             )
         super().save(*args, **kwargs)
+
+
+class WorkSchedule(models.Model):
+    DAYS_OF_WEEK = (
+        ('mon', 'Понедельник'),
+        ('tue', 'Вторник'),
+        ('wed', 'Среда'),
+        ('thu', 'Четверг'),
+        ('fri', 'Пятница'),
+        ('sat', 'Суббота'),
+        ('sun', 'Воскресенье'),
+    )
+
+    realty = models.ForeignKey(
+        Realty,
+        on_delete=models.CASCADE,
+        verbose_name="Объявление",
+        related_name="work_schedule",
+    )
+    day_of_week = models.CharField(
+        max_length=3,
+        choices=DAYS_OF_WEEK,
+        verbose_name="День недели",
+    )
+    start_time = models.TimeField(verbose_name="Начало работы")
+    end_time = models.TimeField(verbose_name="Окончание работы")
+
+    class Meta:
+        verbose_name = "График работы"
+        verbose_name_plural = "Графики работы"
+
+    def __str__(self):
+        return f"{self.get_day_of_week_display()} {self.start_time}-{self.end_time}"
