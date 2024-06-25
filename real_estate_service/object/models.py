e518b8efed8645740cd50a83daf6ea56ed0ec60d
OFF1GHT <kotik.d31sha@yandex.ru>
2024-06-25 19:11:13 +0300

    добавила null и blank, сделала миграции

--- real_estate_service/object/models.py
+++ real_estate_service/object/models.py
@@ -283,8 +283,16 @@ class WorkSchedule(models.Model):
         choices=DAYS_OF_WEEK,
         verbose_name="День недели",
     )
-    start_time = models.TimeField(verbose_name="Начало работы")
-    end_time = models.TimeField(verbose_name="Окончание работы")
+    start_time = models.TimeField(
+        verbose_name="Начало работы",
+        null=True,
+        blank=True,
+    )
+    end_time = models.TimeField(
+        verbose_name="Окончание работы",
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         verbose_name = "График работы"
